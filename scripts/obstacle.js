7108a5ef261731b7ca8684878fe0ad0062c3e915
Noor Wachid <wachid@fisip.net>
2023-03-16 21:57:31 +0700

    Vary the obstacles

--- scripts/obstacle.js
+++ scripts/obstacle.js
@@ -1,13 +1,30 @@
 class Obstacle {
 	constructor(velocity) {
+		this.alive = true;
+
 		this.size = new Vector(
 			Math.random() * 0.1 + 0.1, 
 			Math.random() * 0.2 + 0.2
 		);
 		this.position = new Vector(
-			(Math.random() * 0.8) + globals.viewport.x,
+			(Math.random() * 4) + globals.viewport.x,
 			globals.viewport.y / 3 * 2 - this.size.y,
 		);
+
+		const leftBranchWidth = Math.random() * 0.05 + 0.04;
+		this.leftBranch = {
+			x: -leftBranchWidth,
+			y: Math.random() * this.size.y + 0.1,
+			w: leftBranchWidth,
+			h: Math.random() * 0.1 + 0.08,
+		};
+
+		this.rightBranch = {
+			x: this.size.x,
+			y: Math.random() * this.size.y + 0.1,
+			w: Math.random() * 0.05 + 0.04,
+			h: Math.random() * 0.1 + 0.08,
+		};
 
 		this.velocity = new Vector(velocity, 0);
 
@@ -51,6 +68,26 @@ class Obstacle {
 			this.size.y * globals.inverseWorldScale
 		);
 		ctx.fill();
+
+		ctx.beginPath();
+		ctx.fillStyle = '#333';
+		ctx.rect(
+			(this.position.x + this.leftBranch.x) * globals.inverseWorldScale, 
+			(this.position.y + this.leftBranch.y) * globals.inverseWorldScale, 
+			this.leftBranch.w * globals.inverseWorldScale, 
+			this.leftBranch.h * globals.inverseWorldScale
+		);
+		ctx.fill();
+
+		ctx.beginPath();
+		ctx.fillStyle = '#333';
+		ctx.rect(
+			(this.position.x + this.rightBranch.x) * globals.inverseWorldScale, 
+			(this.position.y + this.rightBranch.y) * globals.inverseWorldScale, 
+			this.rightBranch.w * globals.inverseWorldScale, 
+			this.rightBranch.h * globals.inverseWorldScale
+		);
+		ctx.fill();
 	}
 }
 
@@ -74,18 +111,25 @@ class ObstacleSystem {
 				const velocity = (4 + (globals.level * 0.1)) * -1;
 				this.obstacles.push(new Obstacle(velocity));
 
-				// if (Math.random() > 0.7) {
-				// 	this.obstacles.push(new Obstacle(velocity));
-				// }
+				if (Math.random() > 0.7) {
+					this.obstacles.push(new Obstacle(velocity));
+				}
 			}
 		}
+
+		let dirty = false;
 
 		for (let i = 0; i < this.obstacles.length; ++i) {
 			const index = this.obstacles[i].update(dt, this.player, i);
 
 			if (index > -1) {
-				this.obstacles.splice(index, 1);
+				dirty = true;
+				this.obstacles[index].alive = false;
 			}
+		}
+
+		if (dirty) {
+			this.obstacles = this.obstacles.filter(obstacle => obstacle.alive);
 		}
 	}
 
